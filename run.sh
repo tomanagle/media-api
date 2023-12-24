cd7aee8b2ce4a9e93957264f1622b7bb53ddf80d
Tom Nagle <tomanagle@gmail.com>
2023-12-24 11:06:01 +1000

    fix: rm

--- run.sh
+++
@@ -1,7 +0,0 @@
-export DB_CONNECTION_STRING="mongodb+srv://tom:hbSGJqALXmhzq7DL@snipd.zsbpp.mongodb.net/?retryWrites=true&w=majority"
-
-
-export S3_ACCESS_KEY_ID="AKIASI73ISP6ZLBGSA5H"
-export S3_SECRET_ACCESS_KEY="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"
-
-make watch
